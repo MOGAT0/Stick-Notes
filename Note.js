75c6eef438f2dda5066b2a174639810d53092a86
sam jefferson mogato <124618371+MOGAT0@users.noreply.github.com>
2024-08-21 23:49:57 +0800

    Update Note.js

--- Note.js
+++ Note.js
@@ -1,10 +1,10 @@
 function Note() {
     const [notes, setNotes] = React.useState([]);
-    const [noteField, setNoteField] = React.useState("");
     const [showNotes, setShowNotes] = React.useState(false);
-
-    const handleInputChange = (e) => {
-        setNoteField(e.target.value);
+    let noteField = "";
+    
+    const handleInputChange = (inputField) => {
+        noteField = inputField.target.value;
     };
 
     const handleAddNotes = () => {
@@ -23,14 +23,16 @@ function Note() {
         }
 
         const date = `${gDate.getMonth() + 1}/${gDate.getDate()}/${gDate.getFullYear()}`;
-        const time = `${hour}:${gDate.getMinutes().toString().padStart(2, "0")}:${gDate.getSeconds().toString().padStart(2, "0")} ${abb}`;
-        const timeDate = `${date}, ${time}`;
+        const time = `${hour}:${gDate.getMinutes()}:${gDate.getSeconds()} ${abb}`;
+        console.log(time);
         
-        const newNote = `${noteField}\n${timeDate}`;
+        const timeDate = `${date}, ${time}`;
+        const newNote = `${noteField}-${timeDate}`;
 
         setNotes([...notes, newNote]);
         setShowNotes(false);
         alert("new notes added!");
+        
     };
 
     const handleShowAll = () => {
@@ -40,21 +42,22 @@ function Note() {
     const handleClearNotes = () => {
         setNotes([]);
         setShowNotes(false); 
+        alert('all notes has been cleared!')
     };
 
     return (
         <div>
             <h1>Stick Notes</h1>
-            <input type="text" onChange={handleInputChange} value={noteField} />
+            <input type="text" onChange={handleInputChange} />
             <button onClick={handleAddNotes}>Add note</button>
             <button onClick={handleClearNotes}>Clear notes</button>
             <button onClick={handleShowAll}>Show All notes</button>
             <div>
-                {showNotes && notes.map((n, i) => (
-                    <div key={i}>
-                        {n.split('\n').map((line, j) => (
-                            <p key={j}>
-                                {line}
+                {showNotes && notes.map((myNotes) => (
+                    <div>
+                        {myNotes.split('-').map((NextLine) => (
+                            <p>   
+                                {NextLine}
                                 <br />
                             </p>
                         ))}
